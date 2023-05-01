03541d8eb74869bee1218b1366dece569f33b0a3
Daniel Gooden <dan.gooden.dev@gmail.com>
2023-05-01 22:40:09 +1000

    JS module working with the index.js

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";document.getElementById("content").appendChild(function(){const e=document.createElement("div");return e.appendChild((()=>{const e=document.createElement("div"),t=document.createElement("img"),n=document.createElement("p");return n.textContent="Mellow Mozzarella",e.appendChild(t),e.appendChild(n),e})()),e}())})();

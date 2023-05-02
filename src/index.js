684f74fdce4936aa02b093b0e0e483a8d668ddfb
dangoodie <dan.gooden.dev@gmail.com>
2023-05-02 12:54:11 +1000

    DisplayController working to change the page with JS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import "./assets/styles/style.css";
-import Contact from "./pages/contact";
+
 import Home from "./pages/home.js";
 import Menu from "./pages/menu";
+import Contact from "./pages/contact";
 
 function index() {
   const index = document.createElement("div");
@@ -12,12 +13,40 @@ function index() {
 
 document.body.appendChild(index());
 
-const nav = document.querySelector("nav");
+function DisplayController() {
+  function updateDisplay(menuItem) {
+    document.body.innerHTML = "";
 
-function handleClick(e) {
-  console.log(e.target.attributes.href.value);
+    switch (menuItem) {
+      case "#home":
+        document.body.appendChild(Home());
+        break;
+      case "#menu":
+        document.body.appendChild(Menu());
+        break;
+      case "#contact":
+        document.body.appendChild(Contact());
+        break;
+
+      default:
+        console.error("cannot recognise selection");
+        break;
+    }
+    navEventListeners();
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    const menuItem = e.target.attributes.href.value;
+    updateDisplay(menuItem);
+  }
+
+  function navEventListeners() {
+    const nav = document.querySelector("nav");
+    nav.childNodes.forEach((element) => {
+      element.addEventListener("click", handleClick);
+    });
+  }
+  navEventListeners();
 }
-
-nav.childNodes.forEach((element) => {
-  element.addEventListener("click", handleClick);
-});
+DisplayController();

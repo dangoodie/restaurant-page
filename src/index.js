15753679e5e4fb40e651b699d5c158e9ebcb1b8e
dangoodie <dan.gooden.dev@gmail.com>
2023-05-02 13:43:03 +1000

    menu page basic styling completed

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import "normalize.css"
 import "./assets/styles/style.css";
 import Home from "./pages/home.js";
 import DisplayController from "./displayController";
+import Menu from "./pages/menu";
 
 function index() {
   const contentDiv = document.createElement("div");
   contentDiv.id = "content"
-  contentDiv.appendChild(Home());
+  contentDiv.appendChild(Menu());
 
   return contentDiv;
 }
